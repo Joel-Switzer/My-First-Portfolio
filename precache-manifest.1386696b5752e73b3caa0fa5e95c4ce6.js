0585a381e86e932a1ca247a3839e5ced94a33e2b
Joel Switzer <Joelswitzer1@gmail.com>
2019-02-23 18:40:50 -0600

    Updates

---
+++ precache-manifest.1386696b5752e73b3caa0fa5e95c4ce6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e9c343b7aef990b4f44eac1cc1abac",
+    "url": "/My-First-Portfolio/static/media/icon-responsive.44e9c343.png"
+  },
+  {
+    "revision": "54ed57016a907fce5da4",
+    "url": "/My-First-Portfolio/static/css/main.34be9a72.chunk.css"
+  },
+  {
+    "revision": "185bdb23b441095de2b3",
+    "url": "/My-First-Portfolio/static/js/1.185bdb23.chunk.js"
+  },
+  {
+    "revision": "37d5ffa83c81c0166c8f",
+    "url": "/My-First-Portfolio/static/js/runtime~main.37d5ffa8.js"
+  },
+  {
+    "revision": "c88137724b4048d3a2293407cc93a272",
+    "url": "/My-First-Portfolio/static/media/Resume.c8813772.pdf"
+  },
+  {
+    "revision": "457702ab8f87ece1622ec5150bac5698",
+    "url": "/My-First-Portfolio/static/media/todo-preview.457702ab.png"
+  },
+  {
+    "revision": "05479ae5c4e1eb3cb4115932b418dc23",
+    "url": "/My-First-Portfolio/static/media/this-website-preview.05479ae5.png"
+  },
+  {
+    "revision": "00d62e6ff2aea445d3a4e70a64ea3083",
+    "url": "/My-First-Portfolio/static/media/icon-javascript.00d62e6f.png"
+  },
+  {
+    "revision": "e3d46f21309744f0695a3725641a3bcb",
+    "url": "/My-First-Portfolio/static/media/icon-mysql.e3d46f21.png"
+  },
+  {
+    "revision": "54ed57016a907fce5da4",
+    "url": "/My-First-Portfolio/static/js/main.54ed5701.chunk.js"
+  },
+  {
+    "revision": "dd5da47993596cec12be922061c0a681",
+    "url": "/My-First-Portfolio/static/media/icon-download-green.dd5da479.png"
+  },
+  {
+    "revision": "8a700d6ba550b3584785c8b6ff616f92",
+    "url": "/My-First-Portfolio/static/media/icon-email.8a700d6b.png"
+  },
+  {
+    "revision": "064f0921dce7bf7d40c2e5bac16e40a5",
+    "url": "/My-First-Portfolio/static/media/icon-linked-in.064f0921.png"
+  },
+  {
+    "revision": "4481ca5ab2f2951b215a06c7bf3d22c1",
+    "url": "/My-First-Portfolio/static/media/bg-matrix.4481ca5a.jpg"
+  },
+  {
+    "revision": "17aaf6974d639e82ca239990b8551849",
+    "url": "/My-First-Portfolio/static/media/bg-mobo.17aaf697.jpg"
+  },
+  {
+    "revision": "8f8923a4332c4ddfdfeb88b98b08d1b0",
+    "url": "/My-First-Portfolio/static/media/bg-code-dark.8f8923a4.jpg"
+  },
+  {
+    "revision": "ac0ce996952c30a35732516af0ee48fb",
+    "url": "/My-First-Portfolio/static/media/bg-code.ac0ce996.png"
+  },
+  {
+    "revision": "bb50427db4758da425c0ca7e4ae87a0d",
+    "url": "/My-First-Portfolio/index.html"
+  }
+];
